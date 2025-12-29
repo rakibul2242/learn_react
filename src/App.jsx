9951d7b56e372073e9aa854c856d7e22a0923fb5
rakibul2242 <mdrakibulislam2242@gmail.com>
2025-12-29 15:21:02 +0600

    learning router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import BgChanger from "./BgChanger";
-import Counter from "./Counter";
-import PasswordGenerator from "./PasswordGenerator";
-import Map from "./Map";
-import Clock from "./Clock";
-import NestedLoop from "./NestedLoop";
-import FormSubmitStatus from "./UseFormStatus";
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router";
 
 function App() {
   return (
     <>
-      {/* <BgChanger /> */}
-      {/* <Counter /> */}
-      {/* <PasswordGenerator /> */}
-      {/* <Map /> */}
-      {/* <Clock /> */}
-      {/* <NestedLoop /> */}
-      <FormSubmitStatus/>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider } from "react-router/dom";
+import { createBrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "",
+        element: <div>Home Page</div>,
+      },
+      {
+        path: "about",
+        element: <div>About Page</div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
